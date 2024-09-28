77276fe94aa8264c81c911411c369afcb670f65e
Kacper Szczepan <kacperszczepan@tutanota.com>
2024-09-28 11:31:51 +0200

    init router for landing page

--- hack-client/src/main.tsx
+++ hack-client/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Landing from "./page/Landing";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Landing />,
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
---
+++ hack-client/src/page/Landing.tsx
@@ -0,0 +1,8 @@
+export default function Landing() {
+	return (
+		<div className="w-unit-screen bg-red-950 h-screen p-12">
+			<h1>Social Impact Bridge</h1>
+			<p>lmao</p>
+		</div>
+	);
+}
