765d4732c632e3f913df2ccbb7f3e170882f590e
KariminK <103779342+KariminK@users.noreply.github.com>
2024-09-28 19:45:15 +0200

    removed unnecesary endpoints

--- hack-server/src/controllers/Foundation.ts
+++ hack-server/src/controllers/Foundation.ts
@@ -1,5 +1,17 @@
+import ai from "../models/ChatGPT";
+import Foundation from "../models/Foundation";
+
 const FoundationController = {
     async register(req, res){
+        console.log("Request on foundation register was made");
         let {id, name, email, city, country, projects, target, tags, description} = req.body;
+        if(!description) description = await ai.description(name, target);
+        if(!tags) tags = await ai.tags(name, target);
+        await Foundation.create({
+            id, name, email, city, country, projects, target, tags, description
+        })
+        console.log("Foundation created successfully");
+        res.send();
     }
 }
+export default FoundationController;
--- hack-server/src/routes/foundation.ts
+++ hack-server/src/routes/foundation.ts
@@ -1,5 +1,3 @@
 import { Router } from "express";
 const foundationRouter = Router();
-foundationRouter.post("/register");
-foundationRouter.get("/");
 export default foundationRouter;
