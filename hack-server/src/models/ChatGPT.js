855ec6a2633c0a326b1882e980d4a39d6aef0a5f
KariminK <103779342+KariminK@users.noreply.github.com>
2024-09-29 06:11:08 +0200

    added single foundation funding request endpoint

--- hack-server/src/models/ChatGPT.js
+++ hack-server/src/models/ChatGPT.js
@@ -3,18 +3,35 @@ import dotenv from "dotenv";
 dotenv.config();
 const openai = new OpenAI({ apiKey: process.env.API_KEY });
 const ai = {
-	async description(name, target) {
-		const descriptionCompletion = await openai.chat.completions.create({
-			model: "gpt-3.5-turbo",
-			messages: [
-				{
-					role: "user",
-					content: `Write a description for company ${name} based on this info: ${target}`,
-				},
-			],
-		});
-		const description = descriptionCompletion.choices[0].message.content;
-		return description;
-	},
+  async description(name, target) {
+    const descriptionCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Write a description for company ${name} based on this info: ${target}`,
+        },
+      ],
+    });
+    const description = descriptionCompletion.choices[0].message.content;
+    return description;
+  },
+  async matchToData(data, company) {
+    const dataToMatchCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Find from this data in json: ${JSON.stringify(
+            data
+          )} objects that matches best this company: ${JSON.stringify(
+            company
+          )}. Send response in JSON`,
+        },
+      ],
+    });
+    const matchedObjects = dataToMatchCompletion.choices[0].message.content;
+    return matchedObjects;
+  },
 };
 export default ai;
--- hack-server/src/routes/fundingRequest.js
+++ hack-server/src/routes/fundingRequest.js
@@ -2,5 +2,8 @@ import { Router } from "express";
 import FundingRequestController from "../controllers/fundingRequest.js";
 const fundingRequestRouter = Router();
 fundingRequestRouter.get("/", FundingRequestController.get);
+fundingRequestRouter.get("/:id", FundingRequestController.get);
+
 fundingRequestRouter.post("/", FundingRequestController.add);
+fundingRequestRouter.get("/match", FundingRequestController.getMatched);
 export default fundingRequestRouter;
