ebb51a4878013be28e65a04f0e58686ec9dfa896
KariminK <103779342+KariminK@users.noreply.github.com>
2024-09-29 06:50:17 +0200

    conflicts resolved

--- hack-server/src/controllers/FundingRequest.js
+++ hack-server/src/controllers/FundingRequest.js
@@ -2,18 +2,36 @@ import ai from "../models/ChatGPT.js";
 import FundingRequest from "../models/FundingRequest.js";
 import Company from "../models/Company.js";
 const FundingRequestController = {
-	async get(req, res) {
-		console.log("Request on get funding request was made");
-		const fundingRequests = await FundingRequest.findAll();
-		console.log("request found successfully");
-		res.send(fundingRequests);
-	},
-	async add(req, res) {
-		console.log("Request on add funding request was made");
-		const { title, target, description, FoundationId } = req.body;
-		await FundingRequest.create({ title, target, description, FoundationId });
-		console.log("Funding request added successfully");
-		res.send();
-	},
+  async get(req, res) {
+    const { id } = req.params;
+    console.log("Request on get funding request was made");
+    if (id) {
+      const fundingRequests = await FundingRequest.findAll({
+        where: { FoundationId: id },
+      });
+      console.log("request found successfully");
+      res.send(fundingRequests);
+    } else {
+      const fundingRequests = await FundingRequest.findAll();
+      console.log("request found successfully");
+      res.send(fundingRequests);
+    }
+  },
+  async add(req, res) {
+    console.log("Request on add funding request was made");
+    const { title, target, description, FoundationId } = req.body;
+    await FundingRequest.create({ title, target, description, FoundationId });
+    console.log("Funding request added successfully");
+    res.send();
+  },
+  async getMatched(req, res) {
+    const { id } = req.query;
+    console.log("Request on get matched funding requests was made");
+    const fundingRequests = await FundingRequest.findAll();
+    const company = (await Company.findByPk(id)).dataValues;
+    const result = await ai.matchToData(fundingRequests, company);
+    console.log("request made successfully");
+    res.send(JSON.parse(result));
+  },
 };
 export default FundingRequestController;
--- hack-server/src/routes/fundingRequest.js
+++ hack-server/src/routes/fundingRequest.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import FundingRequestController from "../controllers/FundingRequest.js";
 const fundingRequestRouter = Router();
-fundingRequestRouter.get("/", FundingRequestController.get);
-fundingRequestRouter.get("/:id", FundingRequestController.get);
-
-fundingRequestRouter.post("/", FundingRequestController.add);
 fundingRequestRouter.get("/match", FundingRequestController.getMatched);
+fundingRequestRouter.get("/:id", FundingRequestController.get);
+fundingRequestRouter.get("/", FundingRequestController.get);
+fundingRequestRouter.post("/", FundingRequestController.add);
 export default fundingRequestRouter;
