72a95ee380a739c86649a16cd71c3284906e1ebf
Kacper Szczepan <kacperszczepan@tutanota.com>
2024-09-29 03:39:22 +0200

    update landing page

--- hack-client/src/main.tsx
+++ hack-client/src/main.tsx
@@ -5,13 +5,19 @@ import { createBrowserRouter, Link, RouteObject, RouterProvider } from "react-ro
 import Landing from "./page/Landing";
 import {
 	AppShell,
+	Burger,
+	Text,
 	Button,
 	createTheme,
 	Divider,
+	em,
 	Group,
 	MantineProvider,
+	Skeleton,
 	Title,
 	useMantineColorScheme,
+	NavLink,
+	Box,
 } from "@mantine/core";
 import CreateProfile from "./page/CreateProfile";
 
@@ -23,6 +29,7 @@ import ViewFoundation from "./page/ViewFoundation";
 import ViewCompany from "./page/ViewCompany";
 import useSession from "./page/hook/useSession";
 import Requests from "./page/Requests";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 
 const CONFIG = [
 	{
@@ -63,42 +70,104 @@ const theme = createTheme({
 function MantineRoot() {
 	const scheme = useMantineColorScheme();
 	const session = useSession();
+	const is_mobile = useMediaQuery(`(max-width: ${em(750)})`);
+	const [burger_open, set_burger_opened] = useDisclosure(false);
+
+	if (!is_mobile && burger_open) set_burger_opened.close();
 
 	return (
-		<AppShell header={{ height: 60 }} padding="md">
+		<AppShell
+			header={{ height: 60 }}
+			padding="md"
+			navbar={{ width: 0, breakpoint: "sm", collapsed: { mobile: !burger_open } }}
+		>
 			<AppShell.Header>
 				<Group h="100%" px="md" style={{ justifyContent: "space-between" }}>
-					<Group gap={0}>
-						<Button onClick={() => ROUTER.navigate("/")} variant="transparent">
-							<Title>Impact.site</Title>
-						</Button>
-						<Divider my="sm" orientation="vertical" />
-						<Button variant="transparent" onClick={() => ROUTER.navigate("/requests")}>
-							Financing Requests
-						</Button>
-						<Button variant="transparent" onClick={() => ROUTER.navigate("/profiles")}>
-							Profiles
-						</Button>
-					</Group>
+					{is_mobile ? (
+						<Box>
+							<Burger opened={burger_open} onClick={set_burger_opened.toggle} hiddenFrom="sm" size="sm" />
+							<Button
+								onClick={() => {
+									ROUTER.navigate("/");
+									set_burger_opened.close();
+								}}
+								variant="transparent"
+							>
+								<Title>impact.site</Title>
+							</Button>
+						</Box>
+					) : (
+						<Group gap={0}>
+							<Button onClick={() => ROUTER.navigate("/")} variant="transparent">
+								<Title>impact.site</Title>
+							</Button>
+							<Divider my="sm" orientation="vertical" />
+							<Button variant="transparent" color="gray" onClick={() => ROUTER.navigate("/requests")}>
+								Financing Requests
+							</Button>
+							<Button variant="transparent" color="gray" onClick={() => ROUTER.navigate("/profiles")}>
+								Profiles
+							</Button>
+							<Divider my="sm" orientation="vertical" />
+							<Button
+								variant="transparent"
+								color="gray"
+								onClick={scheme.toggleColorScheme}
+								ml={"xs"}
+								mr={"xs"}
+							>
+								Change theme
+							</Button>
+						</Group>
+					)}
 
 					<Group gap={0}>
-						<Button variant="transparent" onClick={scheme.toggleColorScheme} ml={"xs"} mr={"xs"}>
-							Change theme
-						</Button>
+						<Divider my="sm" orientation="vertical" />
 						{!session ? (
-							<>
-								<Divider my="sm" orientation="vertical" />
-								<Button variant="transparent" onClick={() => ROUTER.navigate("/create-profile")}>
-									Create profile
-								</Button>
-							</>
-						) : undefined}
+							<Button variant="transparent" size="xs" onClick={() => ROUTER.navigate("/create-profile")}>
+								Create profile
+							</Button>
+						) : (
+							<Text>Logged in as {session.name}</Text>
+						)}
 					</Group>
 				</Group>
 			</AppShell.Header>
 			<AppShell.Main>
 				<RouterProvider router={ROUTER} />
 			</AppShell.Main>
+
+			{is_mobile && (
+				<AppShell.Navbar p="md" w={"200px"}>
+					<NavLink
+						variant="transparent"
+						label="Financing Requests"
+						color="blue"
+						onClick={() => {
+							ROUTER.navigate("/requests");
+							set_burger_opened.close();
+						}}
+					/>
+					<NavLink
+						variant="transparent"
+						label="Profiles"
+						color="blue"
+						onClick={() => {
+							ROUTER.navigate("/profiles");
+							set_burger_opened.close();
+						}}
+					/>
+
+					<Divider my="sm" orientation="horizontal" />
+
+					<NavLink
+						variant="transparent"
+						label="Change theme"
+						color="blue"
+						onClick={scheme.toggleColorScheme}
+					/>
+				</AppShell.Navbar>
+			)}
 		</AppShell>
 	);
 }
--- hack-client/src/page/Landing.tsx
+++ hack-client/src/page/Landing.tsx
@@ -1,15 +1,16 @@
-import { AppShell, Group, Burger, Skeleton, Text, Title, Center, List, ListItem, Button } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { AppShell, Group, Burger, Skeleton, Text, Title, Center, List, ListItem, Button, em } from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import { Link } from "react-router-dom";
 import { ROUTER } from "../main";
 import useSession from "./hook/useSession";
 
 export default function Landing() {
 	const session = useSession();
+	const is_mobile = useMediaQuery(`(max-width: ${em(750)})`);
 
 	return (
-		<Center h={"85vh"} style={{ flexDirection: "column" }}>
-			<Title size="10rem">Impact.Site </Title>
+		<Center h={"85vh"} style={{ flexDirection: "column", alignItems: "flex-start" }} p={"lg"}>
+			<Title size={is_mobile ? "2rem" : "10rem"}>impact.site </Title>
 			{!session && <Button onClick={() => ROUTER.navigate("/create-profile")}>Create an account</Button>}
 		</Center>
 	);
